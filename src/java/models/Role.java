ec2f9be1068280a6b31cbcebd73dbd87d727ba90
Matt <Matt@10.0.0.21>
2022-07-11 21:49:08 -0600

    Imported Lab 7 for template.

---
+++ src/java/models/Role.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author 808735
+ */
+public class Role {
+    private int id;
+    private String name;
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
